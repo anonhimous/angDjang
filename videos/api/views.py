0e20a61f0ee713d89283fc6e8ba0c37fa94dc97d
ibrobabs <babskolawole@gmail.com>
2017-05-21 22:38:49 -0600

    model update

--- videos/api/views.py
+++ videos/api/views.py
@@ -8,41 +8,40 @@ from .serializers import VideoSerializer, VideoDetailSerializer
 
 
 class VideoList(generics.ListAPIView):
-	serializer_class = VideoSerializer
-	permission_classes = []
-	authentication_classes = []
+    serializer_class        = VideoSerializer
+    permission_classes      = []
+    authentication_classes  = []
 
-	def get_queryset(self):
-		query = self.request.GET.get("q")
-		if query:
-			# qs = Video.objects.filter(name__icontains=query)
-			qs = Video.objects.search(query)
-		else:
-			qs = Video.objects.all()
-		return qs
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            # qs = Video.objects.filter(name__icontains=query)
+            qs = Video.objects.search(query)
+        else:
+            qs = Video.objects.all()
+        return qs
 
 
 class VideoDetail(generics.RetrieveAPIView):
-    # queryset			 	= Video.objects.all()
-    serializer_class 		= VideoDetailSerializer
-    lookup_field			= 'slug'
-    permission_classes		= []
-    authentication_classes 	= []
+    #queryset                = Video.objects.all()
+    serializer_class        = VideoDetailSerializer
+    lookup_field            = 'slug'
+    permission_classes      = []
+    authentication_classes  = []
 
     def get_queryset(self):
-    	# print("working")
-    	return Video.objects.all()
+        return Video.objects.all()
 
 
 class VideoFeatured(generics.ListAPIView):
-	serializer_class = VideoSerializer
-	permission_classes = []
-	authentication_classes = []
+    serializer_class        = VideoSerializer
+    permission_classes      = []
+    authentication_classes  = []
 
-	def get_queryset(self):
-		query = self.request.GET.get("q")
-		if query:
-			qs = Video.objects.featured().search(query)
-		else:
-			qs = Video.objects.featured()
-		return qs
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            qs = Video.objects.featured().search(query)
+        else:
+            qs = Video.objects.featured()
+        return qs
--- videos/models.py
+++ videos/models.py
@@ -4,54 +4,57 @@ from django.db.models.signals import pre_save
 
 from .utils import unique_slug_generator
 
-
 class VideoQuerySet(models.query.QuerySet):
-	def active(self):
-		return self.filter(active=True)
+    def active(self):
+        return self.filter(active=True)
 
-	def featured(self):
-		return self.filter(featured=True)
+    def featured(self):
+        return self.filter(featured=True)
 
-	def search(self, query):
-		return self.filter(
-				Q(name__icontains=query) |
-				Q(slug__icontains=query) |
-				Q(embed__icontains=query)
-			)
+    def search(self, query):
+        return self.filter(
+                Q(name__icontains=query) |
+                Q(slug__icontains=query) |
+                Q(embed__icontains=query)
+            )
 
-class VideoManager(models.Model):
-	def get_queryset(self):
-		return VideoQuerySet(self.model, using=self._db)
+class VideoManager(models.Manager):
+    def get_queryset(self):
+        return VideoQuerySet(self.model, using=self._db)
 
-	def all(self):
-		return self.get_queryset().active()
+    def all(self):
+        return self.get_queryset().active()
 
-	def featured(self):
-		return self.get_queryset().featured().active()
+    def featured(self):
+        return self.get_queryset().featured().active()
 
-	def search(self, query):
-		return self.get_queryset().search(query).active()
+    def search(self, query):
+        return self.get_queryset().search(query).active()
 
 
 class Video(models.Model):
-	name 		= models.CharField(max_length=120, null=True)
-	slug 		= models.SlugField(unique=True, blank=True)
-	embed 		= models.CharField(max_length=120, help_text='YouTube embed code', null=True, blank=True)
-	active	 	= models.BooleanField(default=True)
-	featured 	= models.BooleanField(default=False)
-	timestamp 	= models.DateTimeField(auto_now_add=True)
+    name        = models.CharField(max_length=220, null=True)
+    slug        = models.SlugField(unique=True, blank=True)
+    embed       = models.CharField(max_length=120, help_text='YouTube embed code', null=True, blank=True)
+    image_path  = models.CharField(max_length=120, default="/static/ang/assets/images/nature/4.jpg", blank=True, null=True)
+    # image2      = models.ImageField(upload_to='images/', null=True, blank=True)
+    active      = models.BooleanField(default=True)
+    featured    = models.BooleanField(default=False)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
-	objects = VideoManager()
+    objects = VideoManager()
 
-	def __str__(self):
-		return self.name
+    def __str__(self): # __unicode__
+        return self.name
 
-	@property
-	def title(self):
-		return self.name
+    @property
+    def title(self):
+        return self.name
+
 
 def video_pre_save_receiver(sender, instance, *args, **kwargs):
-	if not instance.slug:
-		instance.slug = unique_slug_generator(instance)
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
 
 pre_save.connect(video_pre_save_receiver, sender=Video)
